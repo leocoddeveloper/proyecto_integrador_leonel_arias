62e62fe3d97ff55a80341fdf886c3550a2d28368
Leonel Arias <scorpeon2015@gmail.com>
2022-07-05 17:19:13 -0300

    login

---
+++ ProyectoIntegrador_LeonelArias/BackEnd/src/main/java/com/portfolioleo/mgb/Security/Dto/NuevoUsuario.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolioleo.mgb.Security.Dto;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author Usuario
+ */
+public class NuevoUsuario {
+    private String nombre;
+    private String nombeUsuario;
+    private String email;
+    private String password;
+    private Set<String> roles = new HashSet<>();
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombeUsuario() {
+        return nombeUsuario;
+    }
+
+    public void setNombeUsuario(String nombeUsuario) {
+        this.nombeUsuario = nombeUsuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
+
+    public String getNombreUsuario() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+}
